e05b9c9d1ba269db2076622e70dad3ddab08ebcd
choimake <choimake330@gmail.com>
2020-12-28 10:41:19 +0900

    HomeとProductのページ追加とルーティングの設定

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductPage from "./components/pages/HomePage";
+import HomePage from "./components/pages/HomePage";
 
 /**
  * App
@@ -8,22 +9,16 @@ import "./App.css";
  */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/*
+          ルーティングの設定
+         - exactをつけることで、pathへの完全一致でないと指定のcomponentが表示されないようにしている。
+         */}
+        <Route path="/products" component={ProductPage} exact></Route>
+        <Route path="/" component={HomePage} exact></Route>
+      </Switch>
+    </Router>
   );
 }
 
