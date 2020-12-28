bf455496b0000dbe2ed663fe7ff85696778760ad
choimake <choimake330@gmail.com>
2020-12-28 21:56:00 +0900

    GenericTemplateにdrawerとmainを追加

--- src/components/templates/GenericTemplate.js
+++ src/components/templates/GenericTemplate.js
@@ -1,67 +1,260 @@
-import React from "react";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import clsx from "clsx";
+import {
+  createStyles,
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import * as colors from "@material-ui/core/colors";
 import { PropTypes } from "prop-types";
 import {
   AppBar,
+  Box,
+  Container,
   CssBaseline,
+  Divider,
+  Drawer,
   IconButton,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import List from "@material-ui/core/List";
+import { Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
+import HomeIcon from "@material-ui/icons/Home";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const drawerWidth = 240;
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Noto Sans JP",
+      "Lato",
+      "游ゴシック Medium",
+      "游ゴシック体",
+      "Yu Gothic Medium",
+      "YuGothic",
+      "ヒラギノ角ゴ ProN",
+      "Hiragino Kaku Gothic ProN",
+      "メイリオ",
+      "Meiryo",
+      "ＭＳ Ｐゴシック",
+      "MS PGothic",
+      "sans-serif",
+    ].join(","),
+  },
+  palette: {
+    primary: { main: colors.blue[800] }, // テーマの色
+  },
+});
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" to="/">
+        管理画面
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
       display: "flex",
     },
-    toolBar: {
+    toolbar: {
       paddingRight: 24,
     },
-    toolBarIcon: {
+    toolbarIcon: {
       display: "flex",
       alignItems: "center",
       justifyContent: "flex-end",
       padding: "0 8px",
+      ...theme.mixins.toolbar,
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    menuButtonHidden: {
+      display: "none",
     },
     title: {
       flexGrow: 1,
+    },
+    pageTitle: {
+      marginBottom: theme.spacing(1),
+    },
+    drawerPaper: {
+      position: "relative",
+      whiteSpace: "nowrap",
+      width: drawerWidth,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerPaperClose: {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      height: "100vh",
+      overflow: "auto",
+    },
+    container: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      display: "flex",
+      overflow: "auto",
+      flexDirection: "column",
+    },
+    link: {
+      textDecoration: "none",
+      color: theme.palette.text.secondary,
     },
   })
 );
 
 const GenericTemplate = (props) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="absolute">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" area-label="open drawer">
-            <MenuIcon />
-          </IconButton>
-          {/*
-            component h1
-            variant h6
-            h1タグにh6のstyleをあてている
-          */}
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            管理画面
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>{props.title}</main>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={clsx(classes.appBar, open && classes.appBarShift)}
+        >
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(
+                classes.menuButton,
+                open && classes.menuButtonHidden
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            {/*
+              component h1
+              variant h6
+              h1タグにh6のstyleをあてている
+            */}
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
+              管理画面
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <Link to="/" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="トップページ" />
+              </ListItem>
+            </Link>
+            <Link to="/products" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <ShoppingCartIcon />
+                </ListItemIcon>
+                <ListItemText primary="商品ページ" />
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Typography
+              component="h2"
+              variant="h5"
+              color="inherit"
+              noWrap
+              className={classes.pageTitle}
+            >
+              {props.title}
+            </Typography>
+            {props.children}
+            <Box pt={4}>
+              <Copyright />
+            </Box>
+          </Container>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 };
 
 GenericTemplate.propTypes = {
+  children: PropTypes.element,
   title: PropTypes.string,
 };
 
